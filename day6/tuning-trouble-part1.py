3eed4430fc4fb41367ce0d8cb35b61c6eec21d5d
doubleforte <brian@doubleforte.net>
2022-12-18 15:33:35 -0800

    Update Day 6 to use `set`.

--- day6/tuning-trouble-part1.py
+++ day6/tuning-trouble-part1.py
@@ -1,19 +1,16 @@
 # https://adventofcode.com/2022/day/6
 
-import re
-
 with open('day6/data.txt', 'r') as file:
     stream = file.read().strip()
 
 i = 0
 length_of_marker = 4
-pattern = r'^(?:([A-Za-z])(?!.*\1))*$'  # All unique characters?
 
 while i < len(stream):
-    quad = stream[i:length_of_marker + i]
+    group = stream[i:length_of_marker + i]
 
-    if re.match(pattern, quad):
-        print(f'Marker complete at {i + length_of_marker}')  # 1566
+    if len(set(group)) == length_of_marker:
+        print(f'Marker complete at {i + length_of_marker}')  # 2265
         break
 
     i += 1
